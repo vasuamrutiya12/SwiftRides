135d3cb4671b8ccb8f2447280fdf5b929db25885
VASU_ AMRUTIYA_ <vasuamrutiya12@gamil.com>
2025-07-02 12:15:31 +0530

    Updated folders

--- BackEnd/my-springBoot-backend/ApiGateway/src/main/java/com/gateway/security/JwtUtil.java
+++ BackEnd/my-springBoot-backend/ApiGateway/src/main/java/com/gateway/security/JwtUtil.java
@@ -1,32 +1,30 @@
 package com.gateway.security;
 
-
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class JwtUtil {
 
-
-    public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
-
+    @Value("${jwt.secret}")
+    private String secret;
 
     public void validateToken(final String token) {
         Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
     }
 
-
-
     private Key getSignKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        byte[] keyBytes = secret.getBytes(StandardCharsets.UTF_8);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 }
--- BackEnd/my-springBoot-backend/UserService/src/main/java/com/UserService/service/JwtService.java
+++ BackEnd/my-springBoot-backend/UserService/src/main/java/com/UserService/service/JwtService.java
@@ -4,19 +4,20 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class JwtService {
 
-
-    public static final String SECRET = "";
-
+    @Value("${jwt.secret}")
+    private String secret;
 
     public void validateToken(final String token) {
         Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
@@ -40,7 +41,7 @@ public class JwtService {
 
 
     private Key getSignKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        byte[] keyBytes = secret.getBytes(StandardCharsets.UTF_8);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 }
